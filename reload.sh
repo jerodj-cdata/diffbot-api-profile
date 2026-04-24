a6ffdfd750e5009d405edf3f3cddff13d248e81d
jeromechoo <hello@jeromechoo.com>
2026-04-24 15:50:09 -0500

    Removed all SQL filtering syntax, added Person & Place
    * Lack of numerical comparator support and data transforms make it
        incredibly challenging to build in first class SQL support for
        WHERE clauses. For this reason, all filtering shall flow
        through DQL instead.
    * The lightened scope makes it a lot easier to add Article and Place
        types as well.

--- reload.sh
+++ reload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-zip -r ../diffbot-api-profile .
+zip -r ../diffbot-api-profile * -x "../diffbot-api-profile/.git/*" "log*.txt" "CLAUDE.md"
 rm ../diffbot-api-profile.apip
 mv ../diffbot-api-profile.zip ../diffbot-api-profile.apip
